3acc8a325ae2f140ea6bdd53f199dd48cf464f76
Karol Stoiński <karol.stoinski@gmail.com>
2026-01-01 12:44:34 +0100

    Rename `fedora-coreos-vm` module to `coreos-k8s-vm` and update references in Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
 module "k8s-control-plane-1" {
-  source        = "./modules/fedora-coreos-vm"
+  source        = "./modules/coreos-k8s-vm"
   name          = "k8s-control-plane-1"
   vm_ip         = "10.10.1.11"
 }
 
 module "k8s-control-plane-2" {
-  source        = "./modules/fedora-coreos-vm"
+  source        = "./modules/coreos-k8s-vm"
   name          = "k8s-control-plane-2"
   vm_ip         = "10.10.1.12"
 }
 
 module "k8s-control-plane-3" {
-  source        = "./modules/fedora-coreos-vm"
+  source        = "./modules/coreos-k8s-vm"
   name          = "k8s-control-plane-3"
   vm_ip         = "10.10.1.13"
 }
