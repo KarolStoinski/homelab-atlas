38a3e79d4dcbdf2b2b3b49a939ff21567c29e31a
Karol Stoiński <karol.stoinski@gmail.com>
2026-01-03 17:05:50 +0100

    Add `reboot-all-nodes.sh` script: automate reboot process for cluster nodes

---
+++ terraform/reboot-all-nodes.sh
@@ -0,0 +1,6 @@
+ssh -o StrictHostKeyChecking=accept-new core@10.10.1.11 sudo reboot
+ssh -o StrictHostKeyChecking=accept-new core@10.10.1.12 sudo reboot
+ssh -o StrictHostKeyChecking=accept-new core@10.10.1.13 sudo reboot
+ssh -o StrictHostKeyChecking=accept-new core@10.10.1.21 sudo reboot
+ssh -o StrictHostKeyChecking=accept-new core@10.10.1.22 sudo reboot
+ssh -o StrictHostKeyChecking=accept-new core@10.10.1.23 sudo reboot
